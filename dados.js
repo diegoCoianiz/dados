de803de8970cbe09baf5d79190f117a10c4e256d
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2022-12-30 19:22:22 -0300

    new function: estadisticasPorCantidadDeOpciones_rondas

--- dados.js
+++ dados.js
@@ -8,12 +8,15 @@ const estadisticasDelNumero_6 = (opciones, condicion) => {
     }
     cantidadDe_6_porRonda.length >= condicion ? victoriasEn_x_rondas.push(1) : null;
 }
-for (let i = 1; i < 4; i++) {
-    totalDeRondas = 5000
-    console.log(`${totalDeRondas} rondas con ${6 * i} dados\n`)
-    victoriasEn_x_rondas = []
-    for (let rondas = 0; rondas < totalDeRondas; rondas++) {
-        estadisticasDelNumero_6(6 * i, 1 * i)
+const estadisticasPorCantidadDeOpciones_rondas = (cantidad, totalDeRondas) => {
+    for (let i = 1; i < cantidad + 1; i++) {
+        console.log(`${totalDeRondas} rondas con ${6 * i} dados\n`)
+        victoriasEn_x_rondas = []
+        for (let rondas = 0; rondas < totalDeRondas; rondas++) {
+            estadisticasDelNumero_6(6 * i, 1 * i)
+        }
+        console.log("ganás el ", ((victoriasEn_x_rondas.length / totalDeRondas) * 100), "% de las veces\n")
     }
-    console.log("ganás el ", ((victoriasEn_x_rondas.length / totalDeRondas) * 100), "% de las veces\n")
 }
+
+estadisticasPorCantidadDeOpciones_rondas(3 , 10000)
